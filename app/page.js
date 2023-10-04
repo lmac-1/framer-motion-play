6461ac435035f6d45104d7bbf4cff97abae519c3
Lucy Macartney <macartney.lucy@gmail.com>
2023-10-04 09:42:46 -0500

    Updates home pgae with navigation

--- app/page.js
+++ app/page.js
@@ -1,13 +1,31 @@
-import { AnimationGrow } from "./components/AnimationGrow";
-import { AnimationWithKeyFrames } from "./components/AnimationWithKeyFrames";
+import Link from "next/link";
+
+const navigation = [
+  { link: "/scroll/image", label: "Image scroll" },
+  { link: "/scroll/progress", label: "Scroll progress" },
+  { link: "/basic", label: "Basic animations" },
+];
 
 export default function Home() {
   return (
-    <main className="mt-20 flex justify-center">
-      <div className="flex gap-20">
-        <AnimationGrow />
-        <AnimationWithKeyFrames />
-      </div>
+    <main className="mt-20 max-w-lg mx-auto">
+      <h1 className="text-4xl font-semibold text-purple-700">
+        Framer Motion Playground
+      </h1>
+      <nav>
+        <ul className="mt-8">
+          {navigation.map(({ link, label }, index) => (
+            <li key={index} className="mt-4">
+              <Link
+                className="underline underline-offset-2 hover:underline-offset-4 hover:text-purple-700"
+                href={link}
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </main>
   );
 }
