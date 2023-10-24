199932fa07c9f5cbd4f07d48a31dbf2ced532381
Lucy Macartney <macartney.lucy@gmail.com>
2023-10-24 17:43:14 -0500

    cards2 tweaks scroll animation

--- app/(animations)/scroll/cards2/page.jsx
+++ app/(animations)/scroll/cards2/page.jsx
@@ -18,7 +18,21 @@ export default function Page() {
     target: titleContainerRef,
     offset: ["end end", "end start"],
   });
-  const opacityTitle = useTransform(titleProgress, [0, 0.6, 0.75], [1, 1, 0]);
+  const opacityTitle = useTransform(titleProgress, [0, 0.7, 0.9], [1, 1, 0]);
+
+  // card scaling
+  const cardsContainerRef = useRef(null);
+  const { scrollYProgress: cardsProgress } = useScroll({
+    target: cardsContainerRef,
+    offset: ["start 20px", "end 300px"],
+  });
+
+  const scaleCard1 = useTransform(cardsProgress, [0, 1], [1, 0.8]);
+  const scaleCard2 = useTransform(cardsProgress, [0.3, 1], [1, 0.85]);
+  const scaleCard3 = useTransform(cardsProgress, [0.4, 1], [1, 0.9]);
+
+  const yCard2 = useTransform(cardsProgress, [0.3, 0.5], ["16rem", "2rem"]);
+  const yCard3 = useTransform(cardsProgress, [0.5, 0.7], ["18rem", "4rem"]);
 
   return (
     <div className="mx-auto max-w-xl">
@@ -33,21 +47,35 @@ export default function Page() {
           </h1>
         </motion.div>
       </motion.section>
-      <div className="pb-10">
+      <div ref={cardsContainerRef} className="pb-14 border">
         <Card
           title="Lorem ipsum dolor sit amet"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          style={{ transform: `translateY(0rem)` }}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          style={{
+            y: "0rem",
+            scale: scaleCard1,
+          }}
+          index={0}
         />
         <Card
           title="Lorem ipsum dolor sit amet"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          style={{ transform: `translateY(1rem)` }}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          style={{
+            y: yCard2,
+            scale: scaleCard2,
+          }}
+          className="bg-pink-700"
+          index={1}
         />
         <Card
           title="Lorem ipsum dolor sit amet"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          style={{ transform: `translateY(2rem)` }}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          style={{
+            y: yCard3,
+            scale: scaleCard3,
+          }}
+          index={2}
+          className="bg-gray-700"
         />
       </div>
       <LoremIpsum length={2} className="mt-6" />
--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -4,13 +4,14 @@ import { motion } from "framer-motion";
 export const Card = ({ title, description, style, className }) => {
   return (
     <motion.div
-      className={clsx("sticky rounded-2xl p-6 top-20", className)}
+      className={clsx(
+        "sticky rounded-2xl p-10 top-10 border-2 border-purple-700 bg-purple-800 text-white shadow-md",
+        className
+      )}
       style={style}
     >
-      <div className="w-full rounded-3xl border-2 border-purple-700 bg-purple-800 p-16 text-white shadow-md">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p>{description}</p>
-      </div>
+      <h3 className="text-2xl font-semibold">{title}</h3>
+      <p>{description}</p>
     </motion.div>
   );
 };
